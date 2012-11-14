0b997935d1f994b1c182a4019767c0a885c8ebdf
slavavdovichenko <slavav@themidnightcoders.com>
2012-11-14 19:10:28 +0200

    update

--- RTMPStreamComeback/ViewController.m
+++ RTMPStreamComeback/ViewController.m
@@ -35,7 +35,7 @@ static NSString *stream = @"myStream";
     // setup the simultaneous record and playback
     [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayAndRecord error: nil];
     
-    [DebLog setIsActive:YES];
+    //[DebLog setIsActive:YES];
 }
 
 -(void)didReceiveMemoryWarning {
