a475ad9dd1330aa070562b9c368e8775ae201043
slavavdovichenko <slavav@themidnightcoder.com>
2013-02-25 00:53:15 +0200

    audio player refactoring

--- RTMPStreamPlayer/ViewController.m
+++ RTMPStreamPlayer/ViewController.m
@@ -45,8 +45,8 @@
     //hostTextField.text = @"rtmp://10.0.1.132:1935/mediaAppDummy";
     //hostTextField.text = @"rtmp://192.168.2.101:1935/live";
     //hostTextField.text = @"rtmp://192.168.2.63:1935/live";
-    //hostTextField.text = @"rtmp://192.168.2.63:1935/vod";
-    hostTextField.text = @"rtmp://demo.eudata.biz:1935/wcc";
+    hostTextField.text = @"rtmp://192.168.2.63:1935/vod";
+    //hostTextField.text = @"rtmp://demo.eudata.biz:1935/wcc";
     //hostTextField.text = @"rtmp://streaming-dev2.affectiva.com:1935/videorecording-dev2";
     hostTextField.delegate = self;
     
--- RTMPStreamPublisher/ViewController.m
+++ RTMPStreamPublisher/ViewController.m
@@ -43,8 +43,8 @@
     //hostTextField.text = @"rtmp://10.0.2.34:1935/mediaAppDummy";
     //hostTextField.text = @"rtmp://192.168.2.101:1935/live";
     //hostTextField.text = @"rtmp://192.168.2.63:1935/live";
-    //hostTextField.text = @"rtmp://192.168.2.63:1935/videorecording";
-    hostTextField.text = @"rtmp://demo.eudata.biz:1935/wcc";
+    hostTextField.text = @"rtmp://192.168.2.63:1935/videorecording";
+    //hostTextField.text = @"rtmp://demo.eudata.biz:1935/wcc";
     //hostTextField.text = @"rtmp://streaming-dev2.affectiva.com:1935/videorecording-dev2";
     hostTextField.delegate = self;
 
@@ -109,7 +109,7 @@
     //[upstream stream:streamTextField.text publishType:PUBLISH_APPEND];
     
     
-    [upstream setAudioPickingSeconds:0.05f];
+    //[upstream setAudioPickingSeconds:0.05f];
     //[upstream setAudioBitrate:64000];
     
     btnConnect.title = @"Disconnect"; 
