89b1630dd1989566e327c56517ba95676302ae1d
slava.vdovichenko <slavav@themidnightcoders.com>
2013-12-25 13:37:50 +0200

    cleaning

--- scripts/copy.sh
+++
@@ -1,5 +0,0 @@
-cp lib/CommLibiOS/CommLibiOS.a ../ios/lib/CommLibiOS
-cp lib/CommLibiOS/include/* ../ios/lib/CommLibiOS/include
-cp lib/MediaLibiOS/MediaLibiOS.a ../ios/lib/MediaLibiOS
-cp lib/MediaLibiOS/include/* ../ios/lib/MediaLibiOS/include
-
