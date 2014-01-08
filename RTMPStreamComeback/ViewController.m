7a69d0c9e8aaa1716d08737c48f77037999e3033
slava.vdovichenko <slavav@themidnightcoders.com>
2014-01-08 19:41:18 +0200

    in CommLibiOS & MediaLibiOS arm64 & x86_64 architectures  have been added

--- RTMPStreamComeback/ViewController.m
+++ RTMPStreamComeback/ViewController.m
@@ -18,17 +18,17 @@
 //static NSString *host = @"rtmp://10.0.1.33:1935/live";
 //static NSString *host = @"rtmp://192.168.2.63:1935/live";
 //static NSString *host = @"rtmp://192.168.2.101:1935/live";
-//static NSString *host = @"rtmp://192.168.1.102:1935/live";
+static NSString *host = @"rtmp://192.168.1.102:1935/live";
 //static NSString *host = @"rtmp://192.168.2.101:1935/live";
-static NSString *host = @"rtmp://80.74.155.7/live";
+//static NSString *host = @"rtmp://80.74.155.7/live";
 
 //static NSString *stream = @"outgoingaudio_c11";
 //static NSString *stream = @"myStream";
 static NSString *stream = @"slavav";
 
 // cross stream mode
-//static BOOL isCrossStreams = NO;
-static BOOL isCrossStreams = YES;
+static BOOL isCrossStreams = NO;
+//static BOOL isCrossStreams = YES;
 
 
 @interface ViewController () <MPIMediaStreamEvent> {
